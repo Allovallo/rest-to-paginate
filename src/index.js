b9707b1e4b80e61487b527aaaf6adb50f44ad834
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-14 18:06:48 +0200

    files updated_final

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
 import articlesTpl from './templates/articles.hbs';
 import './sass/index.scss';
 import NewsApiService from './js/components/news-service';
+import LoadMoreBtn from './js/components/load-more-btn';
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   articleContainer: document.querySelector('.js-articles-container'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 const newsApiService = new NewsApiService();
 
+console.log(loadMoreBtn);
+
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
 
 function onSearch(e) {
   e.preventDefault();
 
   newsApiService.query = e.currentTarget.elements.query.value;
+
+  if (newsApiService.query === '') {
+    return alert('Введить щось нормальне!');
+  }
+
+  loadMoreBtn.show();
   newsApiService.resetPage();
-  newsApiService.fetchArticles().then(articles => {
-    clearArticlesContainer();
-    appendArticlesMarkup(articles);
-  });
+  clearArticlesContainer();
+  fetchArticles();
 }
 
-function onLoadMore() {
-  newsApiService.fetchArticles().then(appendArticlesMarkup);
+function fetchArticles() {
+  loadMoreBtn.disable();
+  newsApiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    loadMoreBtn.enable();
+  });
 }
 
 function appendArticlesMarkup(articles) {
